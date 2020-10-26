b052343b4b92558be1cd612f856fae035156757f
Mohammad-Agha <hamadaagha123@gmail.com>
2020-10-26 20:48:09 +0200

    Adding the routes

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ require('dotenv/config')
 
 const app = express()
 
+// Connect to database
 connectDB()
 
 app.get('/', (req, res) => {
   res.send('Home route running')
 })
+
+// Middlewares called when using routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 
 const PORT = process.env.PORT || 5000
 
