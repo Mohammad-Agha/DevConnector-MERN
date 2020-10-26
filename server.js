6dbe4c0c2e994a4f09eeb833aa6d8b20a2dced64
Mohammad-Agha <hamadaagha123@gmail.com>
2020-10-26 20:29:32 +0200

    Connecting to mongoDB with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const connectDB = require('./config/db')
+require('dotenv/config')
 
 const app = express()
+
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Home route running')
