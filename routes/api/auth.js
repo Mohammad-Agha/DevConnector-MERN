52117fa02d749c8b42d3a9f184e1148a5c3f7007
Mohammad-Agha <hamadaagha123@gmail.com>
2020-10-27 10:51:07 +0200

    User authentication in the auth route (Login)

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
 const User = require('../../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { check, validationResult} = require('express-validator')
+require('dotenv/config')
 
 /**
  * @route  GET api/auth
@@ -16,6 +20,51 @@ router.get('/', auth, async (req, res) => {
     console.error(error.message)
     res.status(500).send('Server error')
   }
+})
+
+/**
+ * @route  POST api/auth
+ * @desc   Authenticate user & get token
+ * @access Public
+ */
+router.post('/',
+// Middleware 
+[
+  check('email', 'Email should be valid').isEmail(),
+  check('password', 'Password is required').exists()
+], async (req, res) => {
+  const errors = validationResult(req)
+
+  // If there are anything wrong with the validation
+  if(!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+
+  const { email, password } = req.body
+
+  try {
+    // Check if email is valid
+    let user = await User.findOne({ email })
+    if(!user) return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] })
+
+    // Check if password is valid
+    const passwordMatch = await bcrypt.compare(password, user.password)
+    if(!passwordMatch) return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] }) 
+
+    // Return token
+    const payLoad = {
+      user: {
+        id: user.id
+      }
+    }
+    jwt.sign(payLoad, process.env.jwtSecret, (err, token) => {
+      if(err) throw err
+      res.json({ token })
+    })
+
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server error')  
+  }
+
 })
 
 module.exports = router
